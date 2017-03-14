a4f6e11525c565a361d6bb5686258fa255277346
Jérémy Guillon <jeremy.guillon@inria.fr>
2017-03-14 14:48:49 +0100

    Add custom doc and examples
    # Added
    - Toolbox’s .xml descriptor files.
    - Example of doc page `HomePage.m`.
    - Link between the custom doc and demos.

---
+++ bntdemos/Contents.m
@@ -0,0 +1,13 @@
+% Brain Network Toolbox Demos
+% (R2016a) 01-Mar-2017
+%
+%   Type 'demo' at the command line to browse the demos of MATLAB, the
+%   Toolboxes, Simulink, and the Blocksets.
+%
+%   To access the Brain Network Toolbox demos directly, type:
+%
+%      demo toolbox bnt
+%
+%   See also BNT
+
+% Copyright 2015-2017 GUILLON Jeremy
---
+++ bnthelp/HomePage.m
@@ -0,0 +1,16 @@
+%% Example Title
+% Summary of example objective
+%
+%% Getting Started
+% Description of second code block
+%
+% <html>
+%   <h1><a href="../../bntdemos/demos.xml">Examples</a></h1>
+%   <h3><a href="../../bntdemos/demos.xml#Multiplex-Network-Analysis0">Mult Analysis</a></h3>
+% </html>
+%
+% Descr
+%
+
+%%% Def
+% Descr
